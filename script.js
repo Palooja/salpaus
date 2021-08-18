88277bf98326bfb1d371346a1d43326f8b15e7bb
Palooja <88773985+Palooja@users.noreply.github.com>
2021-08-18 10:50:23 +0000

    1

---
+++ script.js
@@ -0,0 +1,75 @@
+function laskutehtava(ekaArvo, tokaArvo){
+  return ekaArvo + tokaArvo;
+}
+
+console.log("täälläollaan");
+
+let ika = 16; // numerot kirjoitetaam ilman lainausmerkkejä
+ika = ika + 1; //integer (int)
+
+let ikakirjaimina = "17"; //string (str)
+
+console.log(ikakirjaimina);
+
+console.log("sinun ikäsi on: " + ika);
+
+let juuvaiei = true; //boolean
+
+console.log(juuvaiei)
+
+let nimi = "keijo";
+
+console.log ("kirjoita isolla;" + nimi.toUpperCase())
+
+console.log("splittausharjoitus:" + nimi.split(" ")[0]);
+
+// While ja for loop:
+
+for(let i=0; i<=10; i++){
+  console.log("loopin iteraatioluku on:" + i);
+}
+
+if(ika > 18 || nimi == "Max Palo-oja"){
+  console.log("onnea olet täysi ikäinen");
+
+}
+
+else if (ika === 18 && nimi == "keijo"){
+  console.log("onnea keijo, olet juuri ja juuri täysi-ikäinen");
+}
+
+
+else{
+  console.error("kasva vielä vähän...")
+}
+
+console.log(laskutehtava(9768, 9875));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+monen
+rivin
+kommentti
+*/
